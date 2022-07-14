ba2f3d40b8121b92e81418a2ffd891070b8a31c6
Anna <karaihr6@gmail.com>
2022-07-14 23:24:17 +0300

    added more-info section

---
+++ src/app/components/more-info/more-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'more-info',
+  templateUrl: './more-info.component.html',
+  styleUrls: ['./more-info.component.scss']
+})
+export class MoreInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
