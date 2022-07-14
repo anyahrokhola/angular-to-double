78f55c1730a2072e08dd36485dd6f0e2cf2926b2
Anna <karaihr6@gmail.com>
2022-07-14 15:01:32 +0300

    added intro

--- src/app/components/intro/intro/intro.component.ts
+++ src/app/components/intro/intro/intro.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-intro',
+  selector: 'intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
